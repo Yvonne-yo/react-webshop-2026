e794b437b2007699de7a11a23f9b224e3596369f
yvop <yvonne_oppstedt@hotmail.com>
2026-05-19 23:58:36 +0200

    Creaded AboutView.jsx and ContactView.jsx for the webshop

---
+++ src/views/ContactView.jsx
@@ -0,0 +1,19 @@
+export default function ContactView() {
+    return(
+        <div className="container mx-auto px-4 py-12">
+            <h1 className="text-font-size-h1 font-black text-text-main mb-4">
+                Contact Us
+            </h1>
+
+            <p className="text-text-muted text-lg max-w-xl leading-relaxed">
+                Do you have questions about our products or your order?
+                The YoYo Webshop team is here to help you. Reach out to us anytime at:
+            </p>
+
+            <div className="mt-4 font-bold text-brand dark:text-brand-light">
+                support@yoyowebshop.com
+            </div>
+
+        </div>
+    )
+}
