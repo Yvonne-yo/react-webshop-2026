7723844e92df8745f1b50356b5ee25500879f9b4
yvop <yvonne_oppstedt@hotmail.com>
2026-05-20 00:02:35 +0200

    App.jsx imports 4 views AboutView, CartView, ContactView and HomeView in preparation for routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import MainLayout from "./layout/MainLayout.jsx"
-// import logoButterflyText from "./assets/YoYo_butterfly_200x52.jpg"
-// import logoButterflyOnly from "./assets/YoYo_butterfly_only_200x199.jpg"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ThemeProvider from "./components/ThemeProvider";
 
-import './App.css'
-import ColorCheck from "./dev/ColorCheck"
+import MainLayout from "./layout/MainLayout";
 
-function App() {
+import AboutView from "./views/AboutView";
+import CartView from "./views/CartView";
+import ContactView from "./views/ContactView";
+import HomeView from "./views/HomeView";
 
 
+export default function App() {
   return (
     <>
-      <div>
-      {/* header component */}
-      {/* header component som innehåller
-                hero section component
-                navigation-bar component som även den kan delas upp i fler komponenter så att
-                                        vissa kan återanvändas i footer 
-                                        länk components
-                                        favikon till favikon och som home-länk här
-                                        menu component with the symbol
-                                        cart component with the symbol
-      */}
-      <header>
-          <p>header component (hero-section and navbar)</p>
-      </header>
-
-      {/* main component will be children */}
-      <main>
-        <hr />
-        <p>color check start</p>
-        <ColorCheck />
-        <p>color check end</p>
-        <hr />
-      </main>
-      
-
-      {/* footer component  ska innehålla text och kanske också länkar t.ex. till about */}
-      <footer><p>footer component</p></footer>
-
-      </div>
+      <ThemeProvider>
+        <BrowserRouter>
+          <MainLayout>
+           <HomeView></HomeView>
+           
+          </MainLayout>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
-  )
+  );
 }
-
-export default App
