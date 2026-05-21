fbc639f8b77b71165b2944f1d71dbf1851594b35
yvop <yvonne_oppstedt@hotmail.com>
2026-05-21 22:58:20 +0200

    productsAPi.js general and categorical data fetch from API dummyjson

---
+++ src/api/productsApi.js
@@ -0,0 +1,40 @@
+// Fetch product information from DummyJSON API service at "https:/dummyjson.com/products"
+
+/* ----- Function 1: Fetches a general block of products ----- */
+export async function getAllProducts(limit = 12, skip = 0) {
+
+    try {
+        const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
+
+        if(!response.ok){
+            throw new Error (`HTTP Error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return(data);
+
+    } catch (error) {
+        console.error("Fetch failure in getAllProducts:", error.message);
+        throw new Error(`Could not load products. (${error.message})`);
+    }    
+}
+
+
+/* ----- Function 2: Fetches products exclusively from one single category ----- */
+export async function getProductsByCategory(categoryName, limit = 12, skip = 0) {
+
+    try {
+        const response = await fetch(`https://dummyjson.com/products/category/${categoryName}?limit=${limit}&skip=${skip}`);
+
+        if(!response.ok){
+            throw new Error (`HTTP Error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return(data);
+
+    } catch (error) {
+        console.error(`Fetch failure in getProductsByCategory for ${categoryName}:`, error.message);
+        throw new Error(`Could not load ${categoryName} products. (${error.message})`);
+    }    
+}
