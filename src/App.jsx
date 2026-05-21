f16be4b08ade492f713231d508603e501495d30f
yvop <yvonne_oppstedt@hotmail.com>
2026-05-21 17:09:25 +0200

    App.jsx added routes matching navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import ThemeProvider from "./components/ThemeProvider";
 
 import MainLayout from "./layout/MainLayout";
 
+import HomeView from "./views/HomeView";
 import AboutView from "./views/AboutView";
 import CartView from "./views/CartView";
 import ContactView from "./views/ContactView";
-import HomeView from "./views/HomeView";
+
 
 
 export default function App() {
@@ -15,8 +16,21 @@ export default function App() {
       <ThemeProvider>
         <BrowserRouter>
           <MainLayout>
-           <HomeView></HomeView>
-           
+            
+            {/* ----- Routes matching navbar links -----  */}
+            <Routes>
+              {/* The root path / to webshop homepage view */}
+              <Route path="/" element={<HomeView />} />
+
+              {/* About and Contact views */}
+              <Route path="/about" element={<AboutView />} />
+              <Route path="/contact" element={<ContactView />} />
+
+              {/* Cart checkout placeholder route */}
+              <Route path="/cart" element={<CartView />} />
+
+            </Routes>
+            
           </MainLayout>
         </BrowserRouter>
       </ThemeProvider>
